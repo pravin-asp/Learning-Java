fb612a0492b0e2baa9f8da117c4b045dbaa527a9
Pravin Saravanabhavan <pravin.cs17@bitsathy.ac.in>
2021-07-28 20:19:00 +0530

    functions

---
+++ dynamciFunction.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class dynamciFunction{
+	public static void calculateTotalMealPrice(double listedMealPrice,
+												double tipRate,
+												double taxRate){
+		double tip = tipRate * listedMealPrice;
+		double tax = taxRate * listedMealPrice;
+		double result = listedMealPrice + tip + tax;
+		System.out.println("Your total meal price is " + result);
+	}
+	public static void main(String[] args){
+		calculateTotalMealPrice(15.0, .2, 0.08);
+		calculateTotalMealPrice(25.0, .2, 0.08);
+	}
+}
