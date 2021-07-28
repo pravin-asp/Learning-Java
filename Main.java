c57c59347fa2b9589df57a486db5f2b7ceebc2f7
Pravin Saravanabhavan <pravin.cs17@bitsathy.ac.in>
2021-07-28 14:45:59 +0530

    simple program

---
+++ Main.java
@@ -0,0 +1,14 @@
+class Main{
+	public static void main(String[] args){
+		int studentAge = 15;
+		double studentGPA = 8.92;
+		char studentFirstInitial = 'A';
+		char studentLastInitial = 'S';
+		boolean hasPerfectAttendance = true;
+		System.out.println(studentAge);
+		System.out.println(studentGPA);
+		System.out.println(studentFirstInitial);
+		System.out.println(studentLastInitial);
+		System.out.println(hasPerfectAttendance);
+	}
+}
