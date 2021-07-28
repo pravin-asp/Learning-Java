73d46c8cdd707c69122ff0d3eae4700f780c7612
Pravin Saravanabhavan <pravin.cs17@bitsathy.ac.in>
2021-07-28 14:46:31 +0530

    practiced programs

---
+++ Concat.java
@@ -0,0 +1,7 @@
+public class Concat{
+	public static void main(String[] args){
+		String studentFirstName = "Pravin";
+		String studentLastName = "A S";
+		System.out.println(studentFirstName + " " + studentLastName);
+	}
+}
