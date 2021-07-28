5562a25b200a6425aa032122a33373f4acbe2af6
Pravin Saravanabhavan <pravin.cs17@bitsathy.ac.in>
2021-07-28 14:44:58 +0530

    indexing in string

---
+++ StringIndex.java
@@ -0,0 +1,7 @@
+public class StringIndex{
+	public static void main(String[] args){
+		String name = "Pravin";
+		char nameFirstLetter = name.charAt(0);
+		System.out.println(nameFirstLetter);
+	}
+}
