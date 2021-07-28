e762980cda066c34ea40a4cc7f2f1c5221bf93de
Pravin Saravanabhavan <pravin.cs17@bitsathy.ac.in>
2021-07-28 14:44:08 +0530

    string program

---
+++ Strings.java
@@ -0,0 +1,8 @@
+//import java.util.*;
+//import java.lang.String;
+public class Strings{
+	public static void main(String[] args){
+		String studentName = "Pravin A S";
+		System.out.println(studentName);
+	}
+}
