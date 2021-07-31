dee409b1e5f54c23f7dd9c2cab327f01709d4a1e
Pravin Saravanabhavan <pravin.cs17@bitsathy.ac.in>
2021-07-31 17:05:01 +0530

    encapsulation handson

---
+++ BankDetails.java
@@ -0,0 +1,10 @@
+public class BankDetails{
+	public static void main(String[] args){
+		BankAccount o1 = new BankAccount("Pravin", 1000);
+		System.out.println(o1.deposit(1350));
+		System.out.println(o1.getBalance());
+		o1.withDraw(300);
+		System.out.println(o1.getBalance());
+		o1.withDraw(3000);
+	}
+}
